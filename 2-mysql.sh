a578f3d25fa8bc0eecd358e0c568ec5c790f93d2
Mariangela Teran <teranmari90@gmail.com>
2023-03-04 14:35:58 +0000

    se modifica script 2-mysql

--- 2-mysql.sh
+++ 2-mysql.sh
@@ -4,11 +4,9 @@
 docker pull mysql:8
 
 # Inicia el contenedor de MySQL
-docker run --name=db -p 3306:3306 -e MYSQL_ROOT_PASSWORD=secret-pw -d mysql:8
+docker run --name db -p 3306:3306 -e MYSQL_ROOT_PASSWORD=secret-pw -d mysql:8
 
-# Descarga la imagen de PHPMyAdmin
-docker pull phpmyadmin
 
 # Inicia el contenedor de PHPMyAdmin
-docker run --name=my-admin -p 82:80 --link db:db -d phpmyadmin
+docker run --name myadmin -p 82:80 --link db:db -d phpmyadmin
 
